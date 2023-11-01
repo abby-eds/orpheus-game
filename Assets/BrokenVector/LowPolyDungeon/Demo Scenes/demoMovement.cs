33121159f78327ea1ad47ca0fce1bff604f64605
Corey S <coreys3@wwu.edu>
2023-11-01 00:05:41 -0700

    added decore!!

--- Assets/BrokenVector/LowPolyDungeon/Demo Scenes/demoMovement.cs
+++ Assets/BrokenVector/LowPolyDungeon/Demo Scenes/demoMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class demoMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float baseSpeed = 12f;
