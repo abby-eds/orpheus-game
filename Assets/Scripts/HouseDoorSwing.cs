4b17fe7a760fadda2263d15e5088cd6959b5d5c1
Abigail-Edwards <86379188+abby-eds@users.noreply.github.com>
2023-11-10 18:00:06 -0800

    more door

--- Assets/Scripts/HouseDoorSwing.cs
+++ Assets/Scripts/HouseDoorSwing.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class HouseDoorSwing : MonoBehaviour
+public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool hasKey = false;
+    public bool nearby = false;
+    private Animator anim;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (nearby && hasKey && Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetBool("Open", true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            nearby = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            nearby = false;
+        }
     }
 }
+
