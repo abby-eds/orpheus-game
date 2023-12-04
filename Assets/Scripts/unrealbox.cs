1997cb144c2133b47a218067a060fdb54e72dcbc
Corey S <coreys3@wwu.edu>
2023-12-04 15:47:08 -0800

    made a new script

---
+++ Assets/Scripts/unrealbox.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unrealbox : Spectral
+{
+    // Start is called before the first frame update
+    private GameObject  obj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   /* protected override void ApplySongOfDead(int level){
+        if (level > 0){
+            //change object to new visibility
+            // do I want it to be only shadows??
+            // do I want it to be a faint blue glow
+            // 
+        }
+        if (level == 0){
+            //not playing
+            // make objecs solid again.
+
+        }
+    }
+*/
+}
