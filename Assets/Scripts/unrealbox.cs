fac2d9d8987a48e8506a6af54f4b731c7753e715
Corey S <coreys3@wwu.edu>
2023-12-05 11:26:07 -0800

    water level lowers

--- Assets/Scripts/unrealbox.cs
+++ Assets/Scripts/unrealbox.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class unrealbox : Spectral
 {
     // Start is called before the first frame update
-    private GameObject  obj;
+
+    public bool turnOn = true;
     void Start()
     {
         
@@ -14,21 +15,24 @@ public class unrealbox : Spectral
     // Update is called once per frame
     void Update()
     {
+        GetComponent<MeshRenderer>().enabled = turnOn;
         
     }
 
-   /* protected override void ApplySongOfDead(int level){
+    public override void ApplySongOfDead(int level){
         if (level > 0){
             //change object to new visibility
             // do I want it to be only shadows??
             // do I want it to be a faint blue glow
-            // 
+            turnOn = false;
+            
         }
         if (level == 0){
             //not playing
             // make objecs solid again.
+            turnOn = true;
 
         }
     }
-*/
+
 }
