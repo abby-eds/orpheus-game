00b60ab723faced05d3fc7a342b592cffb2e3ea6
bennete8 <bennete8@wwu.edu>
2023-12-06 19:58:15 -0800

    added whisps that reveal themselves

--- Assets/Scripts/Interactables/Spectral.cs
+++ Assets/Scripts/Interactables/Spectral.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spectral : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,9 @@ public class Spectral : MonoBehaviour
 
     public virtual void ApplySongOfDead(int level)
     {
-
+        Debug.Log("Default Song of Dead level " + level);
     }
+
+
+
 }
