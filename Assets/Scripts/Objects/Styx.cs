5930201d9c79db4970d66e27591f06ee47e5f146
abby-eds <86379188+abby-eds@users.noreply.github.com>
2023-12-09 16:56:03 -0800

    River Styx edits

--- Assets/Scripts/Objects/Styx.cs
+++ Assets/Scripts/Objects/Styx.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Styx : MonoBehaviour
 {
+    private GameObject player;
+    public Chatter playerChat;
+    public BubbleType bubbleType;
+    public string text;
+    public bool right;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == player && !other.isTrigger)
+        {
+            player.GetComponent<PlayerHealth>().TakeDamage();
+            player.GetComponent<PlayerHealth>().TakeDamage();
+            player.GetComponent<PlayerHealth>().TakeDamage();
+        }
     }
 }
